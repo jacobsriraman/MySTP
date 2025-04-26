688573e807449618d2cabb0f023ce90ff05e34ae
Jacob Sriraman <j.b.sriraman@gmail.com>
2025-04-26 09:57:47 -0600

    Create basic HTM files

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 import compute 
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("input.html")
+    
